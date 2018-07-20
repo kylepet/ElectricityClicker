9840d7609d4ca1cab727463deba62554b89db71d
MileKyle <kylepetkovic@gmail.com>
2018-07-19 19:36:14 -0700

    Template files

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label energyMeter;
+
+    private int clicks = 0;
+
+    public void energyClick(ActionEvent actionEvent) {
+        clicks++;
+
+        energyMeter.setAlignment(Pos.CENTER);
+        energyMeter.setText(clicks + " mAH");
+    }
+}
